f46aff55501195a9d68ac1e3034c82166deb2d9b
soumitriadrita-hub <soumitriadrita@gmail.com>
2026-01-01 18:43:56 +1300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function cutCake() {
+  document.getElementById("leftCake").classList.add("split-left");
+  document.getElementById("rightCake").classList.add("split-right");
+
+  for (let i = 0; i < 30; i++) {
+    const h = document.createElement("div");
+    h.className = "confetti";
+    h.textContent = "💖";
+    h.style.left = Math.random() * 100 + "vw";
+    h.style.top = "-20px";
+    document.body.appendChild(h);
+    setTimeout(() => h.remove(), 3000);
+  }
+
+  setTimeout(() => {
+    window.location.href = "letter.html";
+  }, 2500);
+}
+
+function openLetter() {
+  document.getElementById("hint").style.display = "none";
+  document.getElementById("letterText").style.display = "block";
+}
